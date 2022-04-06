535bab13407cf41ff1b867dac645f3ea624bb76b
dlalba282 <dlalba282@gmail.com>
2022-04-05 20:58:13 -0500

    se incluye herokupropfile, y modifica styles register y login

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-
-app.listen(3000, ()=>{
+//jerocu PORT o si no est[a el 3000]
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor funcionando');
 });
 
